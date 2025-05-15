bf8ac861ea796f25b1025a417d7b184c01a27552
mattft0 <matteofloirat112440@gmail.com>
2025-05-15 11:15:14 +0200

    Fixed path

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mon-portfolio/', // Remplacez par le nom de votre repository
+  base: '/mon-portfolio/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
